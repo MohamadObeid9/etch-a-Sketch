8bac9862e50e9739f1637c5457fca0e7f408ab03
MohamadObeid9 <mohamad.obeid.2@isae.edu.lb>
2024-05-26 00:31:52 +0300

    create the 16*16 grid

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const container = document.querySelector("div");
-container.style.cssText="display:flex;border:1px solid black;gap:5px";
+container.style.cssText="display:flex;flex-wrap:wrap;border:1px solid black;max-width:354px;max-height:357px";
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         const element =  document.createElement("div");
-        element.style.cssText="padding:10px;background:blue;margin:5px;";
+        element.style.cssText="padding:10px;background:blue;margin:1px;";
         container.appendChild(element);
     }
 }
