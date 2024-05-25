3db740c3fb30521322e9df6f7229599009066679
MohamadObeid9 <mohamad.obeid.2@isae.edu.lb>
2024-05-26 01:38:48 +0300

    added the hover effect from stack overflow

--- script.js
+++ script.js
@@ -5,6 +5,17 @@ for (let i = 0; i < 16; i++) {
         const element =  document.createElement("div");
         element.style.cssText="padding:10px;background:blue;margin:1px;";
         container.appendChild(element);
+        document.body.addEventListener('mouseover', e => {
+            let div = e.target.closest('div');
+            if (!div) { return; }
+            div.style.backgroundColor = 'red';
+          });
+
+          document.body.addEventListener('mouseout', e => {
+            let div = e.target.closest('div');
+            if (!div) { return; }
+            div.style.backgroundColor = 'red';
+          });
     }
 }
 
