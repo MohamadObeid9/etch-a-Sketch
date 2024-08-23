08cafa6798f200bc0c7024b3a1c63681cc8fd4b4
MohamadObeid9 <mohamad.obeid.2@isae.edu.lb>
2024-08-24 00:18:18 +0300

    from here , i'll be working on a new branch , to fix all the issue i found

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ const container = document.querySelector("div");
 const button = document.createElement("button");
 button.textContent ="choose a number";
 document.body.insertBefore(button,container);
-container.style.cssText="display:flex;flex-wrap:wrap;border:1px solid black;max-width:354px;max-height:357px";
+container.style.cssText="display:flex;gap:1px;flex-wrap:wrap;border:1px solid black;max-width:337px;max-height:337px";
 
 //creation of the grid of divs and how to color them
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         const element =  document.createElement("div");
-        element.style.cssText="padding:10px;background:blue;margin:1px;";
+        element.style.cssText="padding:10px;background:blue;";
         container.appendChild(element);
 
         //how to color a div when we hover over it
@@ -27,29 +27,30 @@ for (let i = 0; i < 16; i++) {
     }
 }
 
+//the user can choose the number he want
 button.addEventListener("click",()=>{
     let numchosen = null;
     do {
         numchosen = prompt("enter a number between 1 and 100 :");
     } while (numchosen < 1 || numchosen >100);
     container.textContent="";
-    container.style.cssText=`display:flex;flex-wrap:wrap;border:1px solid black;max-width:${numchosen}*${22}px;max-height:${numchosen}*${22}px`;
+    container.style.cssText=`display:flex;justify-content:center;align-items:center;gap:1px;flex-wrap:wrap;border:1px solid black;max-width:(${numchosen}*21+1)px;max-height:(${numchosen}*21+1)px`;
     for (let i = 0; i < numchosen; i++) {
         for (let j = 0; j < numchosen; j++) {
 
             const element =  document.createElement("div");
-            element.style.cssText="padding:10px;background:blue;margin:1px;";
+            element.style.cssText="padding:10px;background:blue;";
             container.appendChild(element);
 
             document.body.addEventListener('mouseover', e => {
                 let div = e.target.closest('div');
-                if (!div) { return; }
+                if (div===container) { return; }
                 div.style.backgroundColor = 'red';
               });
 
               document.body.addEventListener('mouseout', e => {
                 let div = e.target.closest('div');
-                if (!div) { return; }
+                if (div===container) { return; }
                 div.style.backgroundColor = 'red';
               });
         }
