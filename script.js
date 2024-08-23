5b880ce78ef2941ddccad06d806cb314cd4bba26
MohamadObeid9 <mohamad.obeid.2@isae.edu.lb>
2024-08-24 01:30:48 +0300

    created the basic structure of the page

--- script.js
+++ script.js
@@ -1,58 +1,53 @@
-const container = document.querySelector("div");
-const button = document.createElement("button");
-button.textContent ="choose a number";
-document.body.insertBefore(button,container);
-container.style.cssText="display:flex;gap:1px;flex-wrap:wrap;border:1px solid black;max-width:337px;max-height:337px";
+//create the title
+const title = document.createElement("h1");
+title.textContent = "etch a sketch";
+document.body.appendChild(title);
 
-//creation of the grid of divs and how to color them
+//create the button container
+const buttonContainer = document.createElement("div");
+document.body.appendChild(buttonContainer);
+
+//create the buttons
+const button_new_game = document.createElement("button");
+button_new_game.textContent = "new game!";
+button_new_game.setAttribute("class", "button");
+buttonContainer.appendChild(button_new_game);
+
+const button_clear = document.createElement("button");
+button_clear.textContent = "clear";
+button_clear.setAttribute("class", "button");
+buttonContainer.appendChild(button_clear);
+
+const button_black = document.createElement("button");
+button_black.textContent = "black";
+button_black.setAttribute("class", "button");
+buttonContainer.appendChild(button_black);
+
+const button_colorfull = document.createElement("button");
+button_colorfull.textContent = "colorfull";
+button_colorfull.setAttribute("class", "button");
+buttonContainer.appendChild(button_colorfull);
+
+//create the container
+const container = document.createElement("div");
+container.style.cssText =
+  "display:grid;gap:2px;grid-template-rows:repeat(16,30px);grid-template-columns:repeat(16,30px);";
+container.setAttribute("id", "container");
+document.body.appendChild(container);
+
+//create the initial grid
 for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        const element =  document.createElement("div");
-        element.style.cssText="padding:10px;background:blue;";
-        container.appendChild(element);
-
-        //how to color a div when we hover over it
-        document.body.addEventListener('mouseover', e => {
-            let div = e.target.closest('div');//select the nearset div
-            if (div===container) { return; }//if the nearest div is the container div skip
-            div.style.backgroundColor = 'red';// else make the bgc of this div red
-          });
-
-          //how to color a div when we move the mouse out of it
-          document.body.addEventListener('mouseout', e => {
-            let div = e.target.closest('div');//select the nearset div
-            if (div===container) { return; }//if the nearest div is the container div skip
-            div.style.backgroundColor = 'red';// when we move the mouseOut the div , the bgc of this div rest red
-          });
-    }
+  for (let j = 0; j < 16; j++) {
+    const element = document.createElement("div");
+    element.style.cssText = "padding:10px;background:blue";
+    container.appendChild(element);
+  }
 }
 
-//the user can choose the number he want
-button.addEventListener("click",()=>{
-    let numchosen = null;
-    do {
-        numchosen = prompt("enter a number between 1 and 100 :");
-    } while (numchosen < 1 || numchosen >100);
-    container.textContent="";
-    container.style.cssText=`display:flex;justify-content:center;align-items:center;gap:1px;flex-wrap:wrap;border:1px solid black;max-width:(${numchosen}*21+1)px;max-height:(${numchosen}*21+1)px`;
-    for (let i = 0; i < numchosen; i++) {
-        for (let j = 0; j < numchosen; j++) {
-
-            const element =  document.createElement("div");
-            element.style.cssText="padding:10px;background:blue;";
-            container.appendChild(element);
-
-            document.body.addEventListener('mouseover', e => {
-                let div = e.target.closest('div');
-                if (div===container) { return; }
-                div.style.backgroundColor = 'red';
-              });
-
-              document.body.addEventListener('mouseout', e => {
-                let div = e.target.closest('div');
-                if (div===container) { return; }
-                div.style.backgroundColor = 'red';
-              });
-        }
-    }
-});
+//create the footer
+const footer = document.createElement("h2");
+document.body.appendChild(footer);
+const footerLink = document.createElement("a");
+footerLink.setAttribute("href", "https://github.com/MohamadObeid9");
+footerLink.textContent = "made by mohamad";
+footer.appendChild(footerLink);
