96b4350f34f41f2cd09ae572b5c3e1786f64cb84
MohamadObeid9 <mohamad.obeid.2@isae.edu.lb>
2024-08-23 23:31:04 +0300

    fix an issue where when i hover the mouse over the div , the bgc change so when the div is the container nothing change

--- script.js
+++ script.js
@@ -3,24 +3,30 @@ const button = document.createElement("button");
 button.textContent ="choose a number";
 document.body.insertBefore(button,container);
 container.style.cssText="display:flex;flex-wrap:wrap;border:1px solid black;max-width:354px;max-height:357px";
+
+//creation of the grid of divs and how to color them
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         const element =  document.createElement("div");
         element.style.cssText="padding:10px;background:blue;margin:1px;";
         container.appendChild(element);
+
+        //how to color a div when we hover over it
         document.body.addEventListener('mouseover', e => {
-            let div = e.target.closest('div');
-            if (!div) { return; }
-            div.style.backgroundColor = 'red';
+            let div = e.target.closest('div');//select the nearset div
+            if (div===container) { return; }//if the nearest div is the container div skip
+            div.style.backgroundColor = 'red';// else make the bgc of this div red
           });
 
+          //how to color a div when we move the mouse out of it
           document.body.addEventListener('mouseout', e => {
-            let div = e.target.closest('div');
-            if (!div) { return; }
-            div.style.backgroundColor = 'red';
+            let div = e.target.closest('div');//select the nearset div
+            if (div===container) { return; }//if the nearest div is the container div skip
+            div.style.backgroundColor = 'red';// when we move the mouseOut the div , the bgc of this div rest red
           });
     }
 }
+
 button.addEventListener("click",()=>{
     let numchosen = null;
     do {
@@ -30,7 +36,7 @@ button.addEventListener("click",()=>{
     container.style.cssText=`display:flex;flex-wrap:wrap;border:1px solid black;max-width:${numchosen}*${22}px;max-height:${numchosen}*${22}px`;
     for (let i = 0; i < numchosen; i++) {
         for (let j = 0; j < numchosen; j++) {
-            
+
             const element =  document.createElement("div");
             element.style.cssText="padding:10px;background:blue;margin:1px;";
             container.appendChild(element);
@@ -40,7 +46,7 @@ button.addEventListener("click",()=>{
                 if (!div) { return; }
                 div.style.backgroundColor = 'red';
               });
-    
+
               document.body.addEventListener('mouseout', e => {
                 let div = e.target.closest('div');
                 if (!div) { return; }
